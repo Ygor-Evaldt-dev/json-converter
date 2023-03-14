04bb9f429e23eab829c8494383632d25fe860a41
Ygor-Evaldt-dev <evaldtygor@rede.ulbra.br>
2023-03-13 21:30:28 -0300

    adicionado tratativas de erro

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,12 +9,26 @@ function convertValidJson() {
         formatDestin: typeConversionDestin
     }
 
+    if (!json.value) {
+        alerts.warning("Ops!", "Nenhum JSON adicionado na área de texto.");
+        return;
+    }
+    if (typeConversionOrigin === typeConversionDestin) {
+        alerts.info("Ops!", "Nenhuma conversão a ser feita.");
+        return;
+    }
+
     const atualizedJson = makeConversion(params);
     json.value = atualizedJson;
 }
 
 function makeConversion(params) {
     let { input, formatOrigin, formatDestin } = params;
+    const jsonIsValid = validateJson(input.value);
+    if (!jsonIsValid) {
+        alerts.error("Erro!", "JSON Inválido.");
+        return input.value;
+    }
 
     if (formatOrigin === "stringfy" && formatDestin === "parse") {
         return JSON.parse(input.value);
@@ -23,12 +37,44 @@ function makeConversion(params) {
         return JSON.stringify(input.value);
     }
 
-    window.alert("Nenhuma conversão a ser feita");
     return '';
+}
+
+function validateJson(input) {
+    try {
+        JSON.parse(input);
+    } catch (e) {
+        return false;
+    }
+    return true;
 }
 
 function cleanJsonArea() {
     let jsonArea = document.querySelector("#json-area");
     jsonArea.value = '';
+}
+
+const alerts = {
+    "error": function (title, message) {
+        swal({
+            icon: "error",
+            title: title,
+            text: message
+        })
+    },
+    "warning": function (title, message) {
+        swal({
+            icon: "warning",
+            title: title,
+            text: message
+        });
+    },
+    "info": function (title, message) {
+        swal({
+            icon: "info",
+            title: title,
+            text: message
+        });
+    }
 }
 
